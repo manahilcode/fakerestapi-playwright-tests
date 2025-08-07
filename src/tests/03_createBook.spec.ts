7a1a60756ef19c7b5d02ddf35c32ba075fffa1ec
Manahil Adil <manahiladil137@example.com>
2025-08-07 12:39:01 +0500

    Create book

---
+++ src/tests/03_createBook.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+import { BASE_URL, HEADERS, TEST_BOOK } from '../config/secrets';
+
+test('POST create book - Positive', async ({ request }) => {
+  const response = await request.post(`${BASE_URL}/Books`, {
+    headers: HEADERS,
+    data: TEST_BOOK,
+  });
+
+  expect(response.status()).toBe(200);
+
+  const body = await response.json();
+
+  // ✅ Assertions
+  expect(body.title).toBe(TEST_BOOK.title);
+
+  // 📘 Display the created book details
+  console.log('📘 Created Book:', body);
+});
+
